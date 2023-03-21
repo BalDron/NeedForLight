0dc43ab6129d80647b670065897d272c5a439825
baldron <dronchik07@gmail.com>
2023-03-21 17:05:58 +0400

    new game mode for level one

---
+++ Source/NeedForLight/LevelOneGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NeedForLightGameModeBase.h"
+#include "LevelOneGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEEDFORLIGHT_API ALevelOneGameMode : public ANeedForLightGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
